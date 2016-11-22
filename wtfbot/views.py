ed78f2667311cb3801f41e801eddb4da46b76d5a
Ted Scharff <ted@planet.com>
2016-11-21 17:16:46 -0800

    Add /wtf endpoint to query for terms

--- wtfbot/views.py
+++ wtfbot/views.py
@@ -1,7 +1,9 @@
-from flask import render_template, url_for, redirect
+# coding=utf-8
+from flask import render_template, url_for, redirect, request
 from wtfbot import app, db
 from models import Term, Definition
 import datetime
+import json
 
 # @app.route('/', methods=['GET', 'POST'])
 # def index():
@@ -21,3 +23,37 @@ import datetime
 def index():
     terms = Term.query.order_by(db.asc(Term.text))
     return render_template('index.html', terms=terms)
+
+def find_term(search_qry):
+    term = Term.query.filter(Term.text.ilike(search_qry)).first()
+    return term
+
+
+@app.route('/wtf', methods=['POST'])
+def wtf():
+    '''
+    Return a slack response that defines the term.
+    '''
+    data = request.form
+    search = data.get('text')
+
+    term = find_term(search)
+    if term:
+        definition = term.definitions[-1]
+
+        return json.dumps({
+            "response_type": "in_channel",
+            "text": term.text,
+            "attachments": {
+                "text": definition.text
+            }
+
+        })
+    else:
+        return json.dumps({
+            "response_type": "in_channel",
+            "text": u"¯\_(ツ)_/¯",
+            "Attachments": {
+                "text": "Click here to define %s" % search
+            }
+        })
